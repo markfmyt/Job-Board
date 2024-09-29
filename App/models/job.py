2eb2388d0eba6ba2f3ec1be09137d64a6db21630
Mark <74921616+markfmyt@users.noreply.github.com>
2024-09-29 19:33:42 -0400

    Update job.py
    Enhanced readability

--- App/models/job.py
+++ App/models/job.py
@@ -8,4 +8,6 @@ class Job(db.Model):
     description = db.Column(db.Text, nullable=False)
     date_posted = db.Column(db.DateTime, default=datetime.utcnow)
     employer_id = db.Column(db.Integer, db.ForeignKey('employers.id'), nullable=False)
+
+    # Set in such a way that if a Job gets deleted, all the applications for that job gets deleted as well
     applications = db.relationship('Application', backref='job', lazy=True, cascade="all, delete-orphan")
