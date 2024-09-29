483a4f66b84483645fa4ac05c6062e79c9315f3e
Mark <74921616+markfmyt@users.noreply.github.com>
2024-09-29 19:34:00 -0400

    Update job_seeker.py
    Enhanced readability

--- App/models/job_seeker.py
+++ App/models/job_seeker.py
@@ -4,6 +4,8 @@ from .user import User
 class JobSeeker(User):
     __tablename__ = 'job_seekers'
     id = db.Column(db.Integer, db.ForeignKey('users.id'), primary_key=True)
+
+    # Set in such a way that if a JobSeeker gets deleted, their applications gets deleted as well
     applications = db.relationship('Application', backref='job_seeker', lazy=True, cascade="all, delete-orphan")
 
     __mapper_args__ = {
